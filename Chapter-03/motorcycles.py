956523e69ef585b600d7c50a64c5408de07ab6c7
Lucia Talia <25487534+Luzija@users.noreply.github.com>
2026-05-04 13:27:59 -0600

    Add files via upload

---
+++ Chapter-03/motorcycles.py
@@ -0,0 +1,11 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('harley')
+print(motorcycles)
+
+motorcycles.insert(0, 'kawasaki')
+print(motorcycles)
