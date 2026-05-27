6dbc5ba9a410dd9ccf6cb4bb07ab0fe5de6bfc80
Lucia Talia <25487534+Luzija@users.noreply.github.com>
2026-05-27 12:02:27 -0600

    Add files via upload

---
+++ Chapter-03/guest_list.py
@@ -0,0 +1,6 @@
+# List of people I admire
+guests = ["Radia Perlman", "Van Jacobson", "Salvador Dali"]
+
+# Print a dinner invitation for each person
+for guest in guests:
+    print(f"{guest}, I would like to invite you to dinner.")
